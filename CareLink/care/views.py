6753d191bb2863dcaa3f310a1ac0574dca7b38db
trupalgorasiya <trupalgorasiya510@gmail.com>
2025-09-14 11:37:31 +0530

    check migration and connect database

--- CareLink/care/views.py
+++ CareLink/care/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import *
 # Create your views here.
+def home(request):
+    return HttpResponse("welcome to here!!")
--- CareLink/carelink/settings.py
+++ CareLink/carelink/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'care',
 ]
 
 MIDDLEWARE = [
@@ -72,13 +73,23 @@ WSGI_APPLICATION = 'carelink.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carelink',       # database name
+        'USER': 'postgres',     # username
+        'PASSWORD': 'Trupal',  # password you set
+        'HOST': 'localhost',         # or server IP if remote
+        'PORT': '5432',              # default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
